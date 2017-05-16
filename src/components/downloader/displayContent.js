d74c17e5b094861d7fdf766a577dec2a3f885d28
Armageddonas <icsd11175@icsd.aegean.gr>
2017-05-16 15:21:40 +0300

    Code refactoring

--- src/components/downloader/displayContent.js
+++ src/components/downloader/displayContent.js
@@ -1,34 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import InfoList from '../Info/infoList'
 import SearchVideo from '../../containers/downloader/searchVideo'
-import {findUniqueObjectPos} from '../../tools/utilities/arrayUtilities'
 
-class DisplayContent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {videos: []};
+const DisplayContent = ({videos, handleRemoveVideo, downloadPath}) => {
+    return (
+        <div>
+            <SearchVideo/>
+            <br/>
+            <br/>
+            <InfoList videos={videos} handleRemoveVideo={handleRemoveVideo}
+                      downloadPath={downloadPath}/>
+        </div>
+    );
+};
 
-        this.handleRemoveVideo = this.handleRemoveVideo.bind(this);
-    }
-
-    handleRemoveVideo(id) {
-        let index = findUniqueObjectPos(this.state.videos, 'id', id);
-        this.state.videos.splice(index, 1);
-
-        this.setState({videos: this.state.videos});
-    }
-
-    render() {
-        return (
-            <div>
-                <SearchVideo videos={this.state.videos}/>
-                <br/>
-                <br/>
-                <InfoList videos={this.props.videos} handleRemoveVideo={this.props.handleRemoveVideo}
-                      downloadPath={this.props.downloadPath}/>
-            </div>
-        );
-    }
-}
 export default DisplayContent;
