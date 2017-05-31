6c41124d6ad205cf35247bca11962f09491a413c
Armageddonas <icsd11175@icsd.aegean.gr>
2017-05-31 19:18:12 +0300

    Minor changes

--- environment/index.js
+++ environment/index.js
@@ -1,9 +1,5 @@
 const environment = {
-    debug: true,
-    initVideoList: {
-        active: false,
-        sampleUrls: ['https://www.youtube.com/watch?v=momDN8D2pIQ', 'https://www.youtube.com/watch?v=riwxbh_n_WM', 'https://www.youtube.com/watch?v=BQNAWxJ0Hvg', 'https://www.youtube.com/watch?v=LQUXuQ6Zd9w', 'https://www.youtube.com/watch?v=b1B_pZC8aWU']
-    }
+    debug: false
 };
 
 export default environment;
