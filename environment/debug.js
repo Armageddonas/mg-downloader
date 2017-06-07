151f5c5daf9cdcc532ccaee19720febaf083e2cf
Armageddonas <icsd11175@icsd.aegean.gr>
2017-06-07 14:04:55 +0300

    Minor changes

--- environment/debug.js
+++ environment/debug.js
@@ -2,7 +2,7 @@ const environment = {
     debug: true,
     initVideoList: {
         active: false,
-        sampleUrls: ['https://www.youtube.com/watch?v=momDN8D2pIQ', 'https://www.youtube.com/watch?v=riwxbh_n_WM', 'https://www.youtube.com/watch?v=BQNAWxJ0Hvg', 'https://www.youtube.com/watch?v=LQUXuQ6Zd9w', 'https://www.youtube.com/watch?v=b1B_pZC8aWU']
+        sampleUrls: ['https://www.youtube.com/watch?v=7GYJ8N7WSgI', 'https://www.youtube.com/watch?v=LQUXuQ6Zd9w', 'https://www.youtube.com/watch?v=b1B_pZC8aWU']
     }
 };
 
