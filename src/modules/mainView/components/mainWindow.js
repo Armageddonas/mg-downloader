a4961ce4ccb63472dbd41fb7e082bc7de357b756
Armageddonas <icsd11175@icsd.aegean.gr>
2017-06-16 19:42:35 +0300

    Update ytdl component

--- src/modules/mainView/components/mainWindow.js
+++ src/modules/mainView/components/mainWindow.js
@@ -6,6 +6,7 @@ import NavContent from '../containers/navContent'
 import fileManager from '../../../tools/fileManager/fileManager'
 import TitleBar from './titlebar';
 import Overlay from './overlay';
+import YtdlUpdater from './ytdlUpdater';
 
 class MainWindow extends Component {
     constructor(props) {
@@ -59,7 +60,10 @@ class MainWindow extends Component {
                                             <Grid.Column textAlign="center">
                                                 <Header as="h1">MeGa Downloader</Header>
                                             </Grid.Column>
-                                            <Grid.Column mobile={2} tablet={1} computer={1}>
+                                            <Grid.Column mobile={1} tablet={1} computer={1}>
+                                                <YtdlUpdater name="settings" size="large" onClick={this.toggleVisibility}/>
+                                            </Grid.Column>
+                                            <Grid.Column mobile={1} tablet={1} computer={1}>
                                                 <Icon name="settings" size="large" onClick={this.toggleVisibility}/>
                                             </Grid.Column>
                                         </Grid.Row>
